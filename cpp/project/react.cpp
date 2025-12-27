5cccd47f24e6d5397a00b052a2ec537a6bb5005b
Stephen Ly <stephenly@vi-4.local>
2025-12-27 20:53:52 +0700

    reaction time test

--- cpp/project/react.cpp
+++ cpp/project/react.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
 
 using namespace std;
 
 int main() {
     srand(time(NULL));
-    char playAgain;
 
-    cout << "----- Reaction Time Test -----\n";
-    cout << "Press ENTER when you see GO!\n";
+    char playAgainVar;
+    char* playAgain = &playAgainVar; // pointer to playAgain
+    double reactionVar;
+    double* reaction = &reactionVar; // pointer to reaction
 
     do {
         cout << "\nGet ready...\n";
 
-        int waitTime = rand() % 4 + 2; 
+        int waitTime = rand() % 4 + 2; // 2–5 seconds
         time_t startWait = time(NULL);
         while (time(NULL) - startWait < waitTime) {}
 
         cout << "GOOO!!!\n";
 
-        clock_t start = clock();
-        cin.get();   
-        clock_t end = clock();
+        clock_t startVar = clock();
+        clock_t* start = &startVar; // pointer to start
+        cin.get();
+        clock_t endVar = clock();
+        clock_t* end = &endVar;     // pointer to end
 
-        double reaction =
-            double(end - start) / CLOCKS_PER_SEC * 1000;
+        // calculate reaction using pointers
+        *reaction = double(*end - *start) / CLOCKS_PER_SEC * 1000;
 
-        cout << "Your reaction time: " << reaction << " ms\n";
+        cout << "Your reaction time: " << *reaction << " ms\n";
 
         cout << "Play again? (y/n): ";
-        playAgain = cin.get();   
+        *playAgain = cin.get();
+        cin.get(); // remove leftover ENTER
 
-        cin.get(); 
-
-    } while (playAgain == 'y' || playAgain == 'Y');
+    } while (*playAgain == 'y' || *playAgain == 'Y');
 
     cout << "\nThanks for playing!\n";
     return 0;
